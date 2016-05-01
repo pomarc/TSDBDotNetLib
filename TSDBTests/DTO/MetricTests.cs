4a50de506690458a6009eb6217df2e1314ec4ff4
marco poponi <pomarc@gmail.com>
2016-05-01 17:58:54 +0200

    unit test metric, putoptions

---
+++ TSDBTests/DTO/MetricTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TSDBDotNetLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSDBDotNetLib.Tests
+{
+    [TestClass()]
+    public class MetricTests
+    {
+        [TestMethod()]
+        public void MetricTest()
+        {
+            Metric metric = new Metric();
+            metric.Key = "system.key.test";
+            Assert.IsNotNull(metric.Tags);
+            metric.Tags.Add("test", "value");
+            Assert.IsTrue(metric.Tags.Count == 1);
+            
+        }
+
+        [TestMethod()]
+        public void MetricTestInit()
+        {
+            Metric metric = new Metric("system.key.test");
+            Assert.IsTrue(metric.Key == "system.key.test");
+            Assert.IsNotNull(metric.Tags);
+            metric.Tags.Add("test", "value");
+            Assert.IsTrue(metric.Tags.Count == 1);
+        }
+
+        [TestMethod()]
+        public void MetricTestInit2()
+        {
+            Metric metric = new Metric("system.key.test");
+        
+            metric.Tags.Add("test", "value");
+ 
+            Metric metric2 = new Metric(metric, "item");
+            Assert.IsNotNull(metric2.Tags, "tags initialized");
+
+            Assert.IsTrue(metric2.Key == metric.Key + ".item", "well formed composed key");
+            metric2.Tags.Add("test2", "value2");
+
+            Assert.IsTrue(metric2.Tags.Count == 2,"added tags");   
+        }
+
+        [TestMethod()]
+        public void MetricTestInit3()
+        {
+            Metric metric = new Metric("system.key.test");
+
+            metric.Tags.Add("test", "value");
+
+            Metric metric2 = new Metric(metric);
+            Assert.IsNotNull(metric2.Tags,"tags initialized");
+
+            Assert.IsTrue(metric2.Key == metric.Key,"same key");
+            metric2.Tags.Add("test2", "value2");
+
+            Assert.IsTrue(metric2.Tags.Count == 2,"added tag");
+        }
+    }
+}
---
+++ TSDBTests/DTO/PutOptionsTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TSDBDotNetLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSDBDotNetLib.Tests
+{
+    [TestClass()]
+    public class PutOptionsTests
+    {
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            PutOptions putoptions = new PutOptions();
+
+            Assert.IsTrue(putoptions.ToString() == "", "empty");
+
+            putoptions.Details = true;
+
+            Assert.IsTrue(putoptions.ToString() == "?details", "only details");
+
+            putoptions.Summary = true;
+
+            Assert.IsTrue(putoptions.ToString() == "?details"," summary+details, only details");
+
+            putoptions.Details = false;
+
+            Assert.IsTrue(putoptions.ToString() == "?summary","summary only");
+
+
+            putoptions.Sync = true;
+
+
+            Assert.IsTrue(putoptions.ToString() == "?summary&sync", "summary & sync");
+
+            putoptions.Summary = false;
+
+
+            Assert.IsTrue(putoptions.ToString() == "?sync", "only sync");
+
+
+            putoptions.Details = true;
+            Assert.IsTrue(putoptions.ToString() == "?details&sync", "details+ sync");
+        }
+    }
+}
